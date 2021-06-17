9c19b42d78875e6f2b90bead141311f8c8420477
Leela Narasimha <leelanrp@qualminds.com>
2021-06-17 18:51:43 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true
+    },
+    parserOptions: {
+        ecmaVersion: 12,
+        sourceType: 'module'
+    },
+    plugins: ['promise'],
+    rules: {
+        'no-restricted-imports': ['error', 'underscore'],
+        eqeqeq: 'error',
+        'no-console': 'off',
+        'consistent-return': 'error',
+        'no-unused-vars': 'off',
+        indent: ['error', 4],
+        'linebreak-style': ['error', 'unix'],
+        quotes: ['error', 'single'],
+        semi: ['error', 'always']
+    }
+};
